e4cba1ecf98e97ae69e83a48bbdfcf9e8848953d
Austin Fisher <42326201+FishTaleGames@users.noreply.github.com>
2018-09-09 14:59:13 -0700

    Add files via upload

---
+++ ScratchCardController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScratchCardController : MonoBehaviour
+{
+    public Camera MainCamera;
+
+    public ScratchCardMain Card;
+    public ScratchCardEraseProgress Progress;
+
+    public Sprite ScratchSurfaceSprite;
+	public Texture EraseTexture;
+	
+	public GameObject ImageCard;
+	public Shader MaskShader;
+	public Shader BrushShader;
+	public Shader MaskProgressShader;
+	
+	private Material scratchSurfaceMaterial;
+	private Material eraserMaterial;
+	private Material progressMaterial;
+	
+	void Awake()
+	{
+		if (Card.MainCamera == null)
+		{
+			Card.MainCamera = MainCamera;
+		}
+		
+		if (Card.ScratchSurface == null)
+		{
+			scratchSurfaceMaterial = new Material(MaskShader);
+			scratchSurfaceMaterial.mainTexture = ScratchSurfaceSprite.texture;
+			Card.ScratchSurface = scratchSurfaceMaterial;
+		}
+		
+		if (Card.Eraser == null)
+		{
+			eraserMaterial = new Material(BrushShader);
+			eraserMaterial.mainTexture = EraseTexture;
+			Card.Eraser = eraserMaterial;
+		}
+		
+		if (Card.Progress == null)
+		{
+			progressMaterial = new Material(MaskProgressShader);
+			Card.Progress = progressMaterial;
+		}
+		
+		Card.Surface = ImageCard.transform;
+		var image = ImageCard.GetComponent<Image>();
+		image.sprite = ScratchSurfaceSprite;
+		image.material = scratchSurfaceMaterial;
+	}
+	
+	public void SetEraseTexture(Texture texture)
+	{
+		eraserMaterial.mainTexture = texture;
+	}
+
+    public void ResetScratchCard()
+    {
+        Card.Reset();
+    }
+}
---
+++ ScratchCardUI.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScratchCardUI : MonoBehaviour
+{
+    public Button restartButton;
+    public Texture[] Brushes;
+	public Toggle[] BrushToggles;
+	public Toggle ProgressToggle;
+	public Text ProgressText;
+
+    public ScratchCardController CardController;
+    public ScratchCardEraseProgress EraseProgress;
+
+    void Start()
+	{
+		Application.targetFrameRate = 60;
+		ProgressToggle.isOn = PlayerPrefs.GetInt("Toggle", 0) == 0;
+		EraseProgress.OnProgress += OnEraseProgress;
+
+        restartButton.onClick.AddListener(Restart);
+
+		for (var i = 0; i < BrushToggles.Length; i++)
+		{
+			BrushToggles[i].onValueChanged.AddListener(OnChange);
+		}
+		BrushToggles[PlayerPrefs.GetInt("Brush")].isOn = true;
+	}
+
+	public void OnChange(bool val)
+	{
+		for (var i = 0; i < BrushToggles.Length; i++)
+		{
+			if (BrushToggles[i].isOn)
+			{
+                CardController.SetEraseTexture(Brushes[i]);
+				PlayerPrefs.SetInt("Brush", i);
+				break;
+			}
+		}
+	}
+
+	public void OnCheck(bool check)
+	{
+		EraseProgress.gameObject.SetActive(ProgressToggle.isOn);
+		PlayerPrefs.SetInt("Toggle", ProgressToggle.isOn ? 0 : 1);
+	}
+
+	public void Restart()
+	{
+#if UNITY_5_0 || UNITY_5_1 || UNITY_5_2
+		Application.LoadLevel(0);
+#else
+		UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+#endif
+	}
+
+	public void OnEraseProgress(float progress)
+	{
+		ProgressText.text = "Progress: " + Mathf.Round(progress * 100f).ToString() + " %";
+	}
+}
